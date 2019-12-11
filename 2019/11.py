b5ffe6f90f72b64fb2d527db1e29b287975d1293
TFeld00 <tfeld00@gmail.com>
2019-12-11 06:15:19 +0100

    Rewrote a bit

--- 2019/11.py
+++ 2019/11.py
@@ -6,46 +6,33 @@ with open('11.txt','r')as f:
  for l in f:
   L+=map(int,l.split(','))
 
+def paint(background):
+    q=Queue()
+    q.put(background)
+    p={}
+    x,y,d=0,0,0
+    l=dict(enumerate(L))
+    i=0
+    while 1:
+        o=[]
+        r=intcodeUntilRead(l,i,q.get_nowait,o.append)
+        if o:
+            p[(x,y)]=o.pop(0)
+        if o:
+            dd=1 if o.pop(0)else-1
+            d=(d+dd)%4
+            x,y=[(x,y-1),(x+1,y),(x,y+1),(x-1,y)][d]
+            q.put(p.get((x,y),background))
+        if not r:break
+        l,i=r
+    return p
 
-q=Queue()
-q.put(0)
-p={}
-x,y,d=0,0,0
-l=dict(enumerate(L))
-i=0
-while 1:
-    o=[]
-    r=intcodeUntilRead(l,i,q.get_nowait,o.append)
-    if o:
-        p[(x,y)]=o.pop(0)
-    if o:
-        dd=1 if o.pop(0)else-1
-        d=(d+dd)%4
-        x,y=[(x,y-1),(x+1,y),(x,y+1),(x-1,y)][d]
-        q.put(p.get((x,y),0))
-    if not r:break
-    l,i=r
-K=p.keys()
-print len(K)
+#part 1
+p=paint(0)
+print len(p.keys())
 
-q=Queue()
-q.put(1)
-p={}
-x,y,d=0,0,0
-l=dict(enumerate(L))
-i=0
-while 1:
-    o=[]
-    r=intcodeUntilRead(l,i,q.get_nowait,o.append)
-    if o:
-        p[(x,y)]=o.pop(0)
-    if o:
-        dd=1 if o.pop(0)else-1
-        d=(d+dd)%4
-        x,y=[(x,y-1),(x+1,y),(x,y+1),(x-1,y)][d]
-        q.put(p.get((x,y),1))
-    if not r:break
-    l,i=r
+#part 2
+p=paint(1)
 K=p.keys()
 X,Y=zip(*K)
 for j in xrange(min(Y),max(Y)+1):
