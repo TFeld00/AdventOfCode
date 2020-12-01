1bcd2892141ef3fa8c3e16e62bccfe97314ffc56
Thomas Feld <tfe@netcompany.com>
2020-12-01 06:29:31 +0100

    2020: Day 1

--- 2020/1.py
+++ 2020/1.py
@@ -1,5 +1,18 @@
-
+r=[]
 
 with open('1.txt','r')as F:
     for l in F:
-        print (l)
+        r+=int(l),
+        
+# PART 1
+for i,a in enumerate(r):
+    for b in r[i+1:]:
+        if a+b==2020:print(a*b)
+
+
+# PART 2
+for i,a in enumerate(r):
+    for j,b in enumerate(r[i+1:],i+1):
+        for c in r[j+1:]:
+            if a+b+c==2020:print(a*b*c)
+
