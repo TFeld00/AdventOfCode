7ff75450bb8cd5f755461176c106f815371f186a
Thomas Feld <tfe@netcompany.com>
2020-12-02 06:07:15 +0100

    2020: Day 2

---
+++ 2020/2.py
@@ -0,0 +1,16 @@
+import os
+DAY,_,_=os.path.basename(__file__).partition('.')
+
+s=0
+s2=0
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        a,b,c=l.split()
+        x,y=map(int,a.split('-'))
+        b=b[:-1]
+        s+= x<=c.count(b)<=y
+        s2+= (c[x-1:x]==b)^(c[y-1:y]==b)
+
+print(s)
+print(s2)
