1502f9293b5c1555b21e6c096d7a83d62cbf3256
Thomas Feld <tfe@netcompany.com>
2020-12-03 06:12:40 +0100

    2020: Day 3

---
+++ 2020/3.py
@@ -0,0 +1,26 @@
+import os
+DAY,_,_=os.path.basename(__file__).partition('.')
+
+r=[]
+s=0
+i=0
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        r+=[[*l]]
+        s+=l[i%len(l)]=='#'
+        i+=3
+
+print (s)
+
+s2=1
+for dx,dy in (1,1),(3,1),(5,1),(7,1),(1,2):
+    xs=0
+    x=y=0
+    while y<len(r):
+        l=r[y]
+        xs+=l[x%len(l)]=='#'
+        x+=dx
+        y+=dy
+    s2*=xs
+print(s2)
