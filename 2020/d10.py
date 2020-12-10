b822304693f64e0e40346968f101da01d7bd42c8
Thomas Feld <tfe@netcompany.com>
2020-12-10 06:12:34 +0100

    2020: Day 10

---
+++ 2020/d10.py
@@ -0,0 +1,44 @@
+import os
+DAY,_,_=__file__.rpartition('.')
+
+COLS = {
+    '.': (255, 255, 255),
+    '#': (0, 0, 0),
+}
+
+r=[]
+s=0
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        l=int(l)
+        r+=[l]
+        pass
+
+
+R=sorted(r)
+R+=R[-1]+3,
+d1=d2=d3=0
+for a,b in zip([0]+R,R):
+    x=b-a
+    if x==1:d1+=1
+    if x==2:d2+=1
+    if x==3:d3+=1
+print(d1*d3)
+
+d={}
+def f(r):
+    if not r[1:]:return 1
+    v=r[0]
+    if v not in d:
+        s=0
+        if r[1:]and r[1]-v<4:
+            s+=f(r[1:])
+        if r[2:]and r[2]-v<4:
+            s+=f(r[2:])
+        if r[3:]and r[3]-v<4:
+            s+=f(r[3:])
+        d[v]=s
+    return d[v]
+print(f([0]+R))
