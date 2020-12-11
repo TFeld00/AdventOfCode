6184e578d4168dbfd93c7c9e0c6ca26c8b6d69e0
Thomas Feld <tfe@netcompany.com>
2020-12-11 07:55:14 +0100

    Rwrote day 11

--- 2020/d11.py
+++ 2020/d11.py
@@ -1,73 +1,68 @@
 import os
 DAY,_,_=__file__.rpartition('.')
 
-r=[]
+r_orig=[]
 
 with open(f'{DAY}.txt','r')as F:
     for l in F:
         l=l.rstrip('\n')
-        r+=[l]
+        r_orig+=[l]
         pass
 
-r_orig=r[:]
-
-# Part 1
-r=r_orig[:]
-R=[]
-while R!=r:
-    R=r[:]
+def get_neighbors(r,levels=1):
+    D={}
     for i,l in enumerate(r):
-        s=''
         for j,c in enumerate(l):
-            n=0
-            for L in r[max(0,i-1):i+2]:
-                for C in L[max(0,j-1):j+2]:
-                    if C=='#':n+=1
-            if c=='#':n-=1
+            X=[]
+            for dx in -1,0,1:
+                for dy in -1,0,1:
+                    if dx or dy:
+                        x,y=j,i
+                        depth=levels
+                        while depth:
+                            depth-=1
+                            x,y=x+dx,y+dy
+                            if x<0 or y<0 or x>=len(r[0])or y>=len(r):break
+                            C=r[y][x]
+                            if C!='.':
+                                X+=(y,x),;break
+                        D[(i,j)]=X
+    return D
+
+def getRules(part):
+    if part==1:
+        def rule(c,n):
             if c=='L' and n==0:
-                s+='#'
+                return'#'
             elif c=='#' and n>=4:
-                s+='L'
-            else:s+=c
-        R[i]=s
-    R,r=r[:],R[:]
-print(sum(l.count('#')for l in r))
-
-
-# Part 2
-r=r_orig[:]
-
-D={}
-for i,l in enumerate(r):
-    s=''
-    for j,c in enumerate(l):
-        X=[]
-        for dx in -1,0,1:
-            for dy in -1,0,1:
-                if dx or dy:
-                    x,y=j,i
-                    C='.'
-                    while C=='.':
-                        x,y=x+dx,y+dy
-                        if x<0 or y<0 or x>=len(r[0])or y>=len(r):break
-                        C=r[y][x]
-                        if C!='.':
-                            X+=(y,x),;break
-                    D[(i,j)]=X
-R=[]
-while R!=r:
-    R=r[:]
-    for i,l in enumerate(r):
-        s=''
-        for j,c in enumerate(l):
-            n=0
-            for y,x in D[(i,j)]:
-                n+= r[y][x]=='#'
+                return'L'
+            return c
+        return rule
+    if part==2:
+        def rule(c,n):
             if c=='L' and n==0:
-                s+='#'
+                return'#'
             elif c=='#' and n>=5:
-                s+='L'
-            else:s+=c
-        R[i]=s
-    R,r=r[:],R[:]
-print(sum(l.count('#')for l in r))
+                return'L'
+            return c
+        return rule
+
+
+for part,level in (1,1),(2,-1):
+    r=r_orig[:]
+    D=get_neighbors(r,level)
+    rule = getRules(part)
+
+    R=[]
+    while R!=r:
+        R=r[:]
+        for i,l in enumerate(r):
+            s=''
+            for j,c in enumerate(l):
+                n=0
+                for y,x in D[(i,j)]:
+                    n+=r[y][x]=='#'
+                s+=rule(c,n)
+            R[i]=s
+        R,r=r[:],R[:]
+    print(sum(l.count('#')for l in r))
