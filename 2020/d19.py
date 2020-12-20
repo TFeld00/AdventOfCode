800679ff957ad801dac33a3c8621a6b0f3a97f2d
Thomas Feld <tfe@netcompany.com>
2020-12-20 05:57:03 +0100

    "simplified" day 19, and removed unused imports

--- 2020/d19.py
+++ 2020/d19.py
@@ -1,19 +1,7 @@
 import os
 DAY,_,_=__file__.rpartition('.')
 
-from math import *
 import re
-from sympy import *
-from img.img import read_img,write_img  #write_img(DAY,COLS)
-from itertools import *
-from collections import *
-from alg.dijkstra import dijkstra
-from functools import *
-from fractions import *
-import heapq
-from string import * # ascii_letters,ascii_lowercase,ascii_uppercase,digits
-from textwrap import wrap
-from datetime import *
 
 COLS = {
     '.': (255, 255, 255),
@@ -37,21 +25,20 @@ for v in r1:
     
     d[a]=b.replace('"','')
 
-from re import findall,search,sub,match
 def get(n):
     r=d[n]
     
-    if findall(r'\d',r):
-        r=sub(r'\d+',lambda m:'('+get(m[0])+')',r)
+    if re.findall(r'\d',r):
+        r=re.sub(r'\d+',lambda m:'('+get(m[0])+')',r)
     d[n]=r.replace(' ','')
-    d[n]=sub(r'\(([a-z]+)\)',r'\1',d[n])
+    d[n]=re.sub(r'\(([a-z]+)\)',r'\1',d[n])
     return d[n]
 
 rule=get('0')
 
 s=0
 for v in r2:
-    if match('^('+rule+')$',v):s+=1
+    if re.match('^('+rule+')$',v):s+=1
 print(s)
 
 d={}
@@ -61,17 +48,11 @@ for v in r1:
     d[a]=b.replace('"','')
 
 d['8']='(42)+'
-d['11']='(42 31)\
-        |(42 42 31 31)\
-        |(42 42 42 31 31 31)\
-        |(42 42 42 42 31 31 31 31)\
-        |(42 42 42 42 42 31 31 31 31 31)\
-        |(42 42 42 42 42 42 31 31 31 31 31 31)\
-        |(42 42 42 42 42 42 42 31 31 31 31 31 31 31)'
+d['11']='(42 (42 (42 (42 (42 (42 31)? 31)? 31)? 31)? 31)? 31)'
 
 rule=get('0')
 
 s=0
 for v in r2:
-    if match('^('+rule+')$',v):s+=1
+    if re.match('^('+rule+')$',v):s+=1
 print(s)
