d0143770f076405f18256c2077c4911edbe4b47a
TFeld00 <tfeld00@gmail.com>
2021-12-06 05:50:15 +0100

    2021 - Day 05 Removed unused stuff imports

--- 2021/d05.py
+++ 2021/d05.py
@@ -1,50 +1,21 @@
-import os
 DAY,_,_=__file__.rpartition('.')
-
-from itertools import *
-from collections import *
-_pow = pow
-from math import *
-pow = _pow
-from string import ascii_lowercase, ascii_uppercase, ascii_letters, digits, hexdigits, whitespace, punctuation, printable
-from alg.dijkstra import dijkstra
-from img.img import read_img, write_img     #write_img(DAY,COLS)
-from alg.util import parse_with_headers, parse_skip_headers, parse_no_headers
-from functools import *
-from fractions import *
-from textwrap import wrap
-from datetime import *
-from queue import *
-import heapq
-from functools import cache
-import sympy    #sympy.primefactors
-import re
-import sys
-
-COLS = {
-    '.': (255, 255, 255),
-    '#': (0, 0, 0),
-}
 
 from alg.file import download_input
 download_input(DAY)
 
 r=[]
-s=0
+
 
 with open(f'{DAY}.txt','r')as F:
     for l in F:
         l=l.rstrip('\n')
-        #l=map(int,l.split())
         a,_,b=l.split()
-        #l=int(l)
-        #l=list(l)
+
         x,y=map(int,a.split(','))
         X,Y=map(int,b.split(','))
-        #print(l)
+
         r+=[((x,y),(X,Y))]
         pass
-#W,H=len(r[0]),len(r)
 
 
 a=[[0]*1000 for _ in range(1000)]
