aab33964f407436c0dc7384534a4573a0951e299
TFeld00 <tfeld00@gmail.com>
2021-12-06 06:17:46 +0100

    2021 - Day 06 - Shorter variant

--- 2021/d06.py
+++ 2021/d06.py
@@ -1,5 +1,6 @@
 DAY,_,_=__file__.rpartition('.')
 
+
 from alg.file import download_input
 download_input(DAY)
 
@@ -12,18 +13,32 @@ with open(f'{DAY}.txt','r')as F:
         r+=l
         pass
 
-l=[0]*10
+l=[0]*9
 for f in r:
     l[f]+=1
 
 for g in range(256):
-    n=[0]*10
+    n=[0]*9
     for i,f in enumerate(l):
         if i:
             n[i-1]+=f
         else:
             n[6]+=f
             n[8]+=f
+    l=n
+    if g==79: #part 1
+        print(sum(n))
+print(sum(n))
+
+# Variant
+l=[0]*9
+for f in r:
+    l[f]+=1
+
+for g in range(256):
+    n=l[1:]+[0]
+    n[6]+=l[0]
+    n[8]+=l[0]
     l=n
     if g==79: #part 1
         print(sum(n))
