f8328671c40b404614e7633d5dafea5025ac6ea0
TFeld00 <tfeld00@gmail.com>
2021-12-14 08:27:11 +0100

    2021 - Day 14 - simpler count

--- 2021/d14.py
+++ 2021/d14.py
@@ -1,7 +1,7 @@
 DAY,_,_=__file__.rpartition('.')
 
 from alg.util import parse_no_headers
-
+from collections import Counter
 from alg.file import download_input
 download_input(DAY)
 
@@ -22,23 +22,13 @@ for l in r:
     a,b=l.split(' -> ')
     d[a]=b
 
-def count(D):
-    c={}
-    for a,b in D:
-        c[a]=c.get(a,0)+D[a+b]
-        c[b]=c.get(b,0)+D[a+b]
-    for v in c:
-        c[v]=(c[v]+1)//2
-
-    l=[(c[v],v)for v in c]
-    a,b=max(l),min(l)
-    print(a[0]-b[0])
-
 D={}
 for a,b in zip(s,s[1:]):
     c=a+b
     D[c]=D.get(c,0)+1
     
+C=Counter(s)
+
 for t in range(40):
     D2={}
     for v in D:
@@ -47,8 +37,8 @@ for t in range(40):
             c=d[v]
             D2[a+c]=D2.get(a+c,0)+D[v]
             D2[c+b]=D2.get(c+b,0)+D[v]
-        else:
-            D2[v]=D[v]
+            C[c]+=D[v]
     D=D2
     if t in (9,39):
-        count(D)
+        a,*_,b=C.most_common()
+        print(a[1]-b[1])
