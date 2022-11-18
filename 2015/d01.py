ae9a7fa83897a389992e90f4fe0db0cda48e8d89
TFeld00 <tfeld00@gmail.com>
2022-11-18 21:09:14 +0100

    Cleaned up 2015 first days

--- 2015/d01.py
+++ 2015/d01.py
@@ -1,33 +1,5 @@
 DAY,_,_=__file__.rpartition('.')
 
-import os
-from itertools import *
-from collections import *
-_pow = pow
-from math import *
-pow = _pow
-from string import ascii_lowercase, ascii_uppercase, ascii_letters, digits, hexdigits, whitespace, punctuation, printable
-from alg.dijkstra import dijkstra
-from img.img import read_img, write_img, write_img_fromlist     #write_img(DAY,COLS)
-from alg.util import parse_with_headers, parse_skip_headers, parse_no_headers, get_neigbors_both, get_neigbors_diag, get_neigbors_orto
-from alg.floodfill import fill
-from alg.cellular import step_dict, step_list, to_dict, to_lists
-from functools import *
-from fractions import *
-from textwrap import wrap
-from datetime import *
-from queue import *
-import heapq
-from functools import cache
-import sympy    #sympy.primefactors
-import re
-import sys
-
-COLS = {
-    '.': (255, 255, 255),
-    '#': (0, 0, 0),
-}
-
 from alg.file import download_input
 download_input(DAY)
 
