5dcd8b1b51636b0adb279d70dd86f8e0bf10d0c3
TFeld00 <tfeld00@gmail.com>
2022-11-18 21:30:07 +0100

    2015 - Day 05

---
+++ 2015/d05.py
@@ -0,0 +1,66 @@
+DAY,_,_=__file__.rpartition('.')
+
+import os
+from itertools import *
+from collections import *
+_pow = pow
+from math import *
+pow = _pow
+from string import ascii_lowercase, ascii_uppercase, ascii_letters, digits, hexdigits, whitespace, punctuation, printable
+from alg.dijkstra import dijkstra
+from img.img import read_img, write_img, write_img_fromlist     #write_img(DAY,COLS)
+from alg.util import parse_with_headers, parse_skip_headers, parse_no_headers, get_neigbors_both, get_neigbors_diag, get_neigbors_orto
+from alg.floodfill import fill
+from alg.cellular import step_dict, step_list, to_dict, to_lists
+from functools import *
+from fractions import *
+from textwrap import wrap
+from datetime import *
+from queue import *
+import heapq
+from functools import cache
+import sympy    #sympy.primefactors
+import re
+import sys
+
+COLS = {
+    '.': (255, 255, 255),
+    '#': (0, 0, 0),
+}
+
+from alg.file import download_input
+download_input(DAY)
+
+r=[]
+n=0
+s=''
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        #l=[*map(int,l.split())]
+        #l=[*map(int,l)]
+        #l=l.split()
+        #l=int(l)
+        #l=list(l)
+        #print(l)
+        r+=[l]
+        pass
+#W,H=len(r[0]),len(r)
+
+
+import re
+
+#part 1
+n=0
+for s in r:
+    if len(re.findall(r'[aeiou]',s))>=3 and re.search(r'(.)\1',s) and (not re.search(r'ab|cd|pq|xy',s)):
+        n+=1
+print(n)
+
+#part 2
+n=0
+for s in r:
+    if re.search(r'(..).*\1',s) and re.search(r'(.).\1',s):
+        n+=1
+print(n)
