5da3ccbb19913d9c35b1cedbe0af8fec85a4f4a6
TFeld00 <tfeld00@gmail.com>
2022-11-19 20:22:36 +0100

    2015 - Day 08

---
+++ 2015/d08.py
@@ -0,0 +1,21 @@
+DAY,_,_=__file__.rpartition('.')
+
+from alg.file import download_input
+download_input(DAY)
+
+r=[]
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        r+=[l]
+
+a,b=0,0
+c=0
+for s in r:
+    a+=len(s)
+    b+=len(eval(s))
+    c+=len(s)+s.count('"')+s.count('\\')+2
+
+print(a-b) #part 1
+print(c-a) #part 2
