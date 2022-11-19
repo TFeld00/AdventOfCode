c8bf80ff95b9064f99dba6716783ff471800b295
TFeld00 <tfeld00@gmail.com>
2022-11-19 20:27:01 +0100

    2015 - Day 09

---
+++ 2015/d09.py
@@ -0,0 +1,29 @@
+DAY,_,_=__file__.rpartition('.')
+
+from itertools import *
+from alg.file import download_input
+download_input(DAY)
+
+d={}
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        
+        a,_,b,_,c=l.split()
+        c=int(c)
+        d[a]=d.get(a,{})
+        d[a][b]=c
+        d[b]=d.get(b,{})
+        d[b][a]=c
+
+r=d.keys()
+        
+l=[]
+for c in permutations(r):
+    x=sum(d[a][b]for a,b in zip(c,c[1:]))
+    l+=x,
+
+print(min(l))
+
+print(max(l))
