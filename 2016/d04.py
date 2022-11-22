e2372660051ecb6c0ae28896daa3c771bb52b320
TFeld00 <tfeld00@gmail.com>
2022-11-22 12:05:40 +0100

    2016 - Day 04

---
+++ 2016/d04.py
@@ -0,0 +1,33 @@
+DAY,_,_=__file__.rpartition('.')
+
+from collections import *
+from string import ascii_lowercase
+
+from alg.file import download_input
+download_input(DAY)
+
+r=[]
+n=0
+s=''
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        a,b=l.rsplit('-',1)
+        b,c=b[:-1].split('[')
+        r+=[(a,int(b),c)]
+        pass
+
+#part 1
+s=0
+for a,b,c in r:
+    m=Counter(sorted(a.replace('-',''))).most_common(5)
+    if c==''.join(x for x,_ in m):
+        s+=b
+print(s)
+
+#part 2
+for a,b,c in r:
+    a=''.join(a.translate(str.maketrans(ascii_lowercase+'-', ascii_lowercase[b%26:]+ ascii_lowercase[:b%26]+' ')))
+    if 'pole' in a:
+        print(a,b)
