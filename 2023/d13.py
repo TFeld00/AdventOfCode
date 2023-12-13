c3cfdcc01b64d2688f89d7c38e666657aa556e1b
TFeld00 <tfeld00@gmail.com>
2023-12-13 13:14:48 +0100

    2023 - Day 13, a lot faster

--- 2023/d13.py
+++ 2023/d13.py
@@ -11,37 +11,21 @@ with open(f'{DAY}.txt','r')as F:
     for l in F:
         l=l.rstrip('\n')
         r+=[l]
-
-L=parse_no_headers(r)
-        
-def f(M,ignore=0):
+     
+def f(M,target=0):
     for (v,m) in (1,M),(100,[*zip(*M)]):
         for i in range(1,len(m[0])):
-            if all(all(x==y for x,y in zip(l[:i][::-1],l[i:])) for l in m):
-                r=v*i
-                if r!=ignore:
-                    return r
+            s=sum(sum(x!=y for x,y in zip(l[:i][::-1],l[i:])) for l in m)
+            if s==target:
+                return v*i
     return 0
 
 s1=0
 s2=0
-def g(m):
-    global s1
-    x=f(m)
-    s1+=x
-    for i,l in enumerate(m):
-        for j,c in enumerate(l):
-            w=[[*x]for x in m]
-            w[i][j]='.#'[c=='.']
-            q=f(w,x)
-            if q:return q
-    return 0
-import time
-a=time.time()
-for m in L:
-    m=[*map(list,m)]
-    s2+=g(m)
-b=time.time()
-print(b-a)
+
+for m in parse_no_headers(r):
+    s1+=f(m,0)
+    s2+=f(m,1)
+
 print(s1)
 print(s2)
