b5dfc9b2d4a3f11178e8ac67f472d44abf0df175
TFeld00 <tfeld00@gmail.com>
2024-12-03 06:13:15 +0100

    Dayu 4 simplification

--- 2024/d03.py
+++ 2024/d03.py
@@ -13,26 +13,22 @@ with open(f'{DAY}.txt','r')as F:
         t+=l
         
 # Part 1
-
 def mul(a,b):
     return a*b
 
 s=0
-for v in re.findall('mul\(-?\d+,-?\d+\)',t):
+for v in re.findall('mul\(\d+,\d+\)',t):
     s+=eval(v)
 print(s)
 
 # Part 2
 d=1
-def mul(a,b):
-    return a*b if d else 0
-
 s=0
-for v in re.findall('mul\(-?\d+,-?\d+\)|do\(\)|don\'t\(\)',t):
+for v in re.findall('mul\(\d+,\d+\)|do\(\)|don\'t\(\)',t):
     if v=="don't()":
         d=0
     elif v=='do()':
         d=1
-    else:
+    elif d:
         s+=eval(v)
 print(s)
