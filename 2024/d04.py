ff841128cff80bac11278ad09b1f4ebf7aaebe55
TFeld00 <tfeld00@gmail.com>
2024-12-04 10:08:17 +0100

    simplified

--- 2024/d04.py
+++ 2024/d04.py
@@ -1,6 +1,5 @@
 DAY,_,_=__file__.rpartition('.')
 
-from alg.util import rotate90clockwise, rotate180, rotate90counterclockwise
 from alg.file import download_input
 from alg.grid import word_search, pattern_search
 
@@ -12,8 +11,6 @@ with open(f'{DAY}.txt','r')as F:
     for l in F:
         l=l.rstrip('\n')
         r+=[l]
-        
-W,H=len(r[0]),len(r)
         
 # Part 1
 print(word_search(r, 'XMAS', includeDiagonals=True))
