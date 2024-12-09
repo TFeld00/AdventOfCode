343872d92f4e350bf0406b18b41a91f730b2f5db
TFeld00 <tfeld00@gmail.com>
2024-12-09 06:36:57 +0100

    2024 - Day 09

---
+++ 2024/d09.py
@@ -0,0 +1,71 @@
+DAY,_,_=__file__.rpartition('.')
+
+
+from alg.file import download_input
+download_input(DAY)
+
+t=''
+
+with open(f'{DAY}.txt','r')as F:
+    for l in F:
+        l=l.rstrip('\n')
+        t+=l
+        
+s=0
+l=[]
+f=0
+for i,c in enumerate(t):
+   f^=1
+   if f:
+      l+=[i//2]*int(c)
+   else:
+       l+=[-1]*int(c)
+
+i=0
+while i<len(l):
+    c=l[i]
+    if c<0:
+        while l[-1]<0:l.pop()
+        if i>=len(l):break
+        l[i]=l.pop()
+    
+    s+=i*l[i]
+    i+=1
+print(s)
+
+# Part 2
+
+l=[]
+f=0
+for i,c in enumerate(t):
+   f^=1
+   i//=2
+   if f:
+      l+=[int(c),i],
+   else:
+       l+=[int(c),-1],
+
+j=len(l)-1
+while j>0:
+    c,i = l[j]
+    if i<0:j-=1;continue
+    for v,(a,b) in enumerate(l[:j]):
+        if b<0 and a>=c:
+            l[j]=[c,-1]
+            r=[[c,i]]
+            if a>c:r+=[[a-c,-1]];j+=1
+            l[v:v+1]=r
+            while j>0 and j<len(l) and l[j-1][1]<0 and l[j][1]<0: # combine empty
+                l[j-1][0]+=l[j][0]
+                l.pop(j)
+            break
+    j-=1
+
+s=0
+w=0
+for c,i in l:
+    if i>=0:
+        for j in range(w,w+c):
+            s+=i*j
+    w+=c
+print(s)
